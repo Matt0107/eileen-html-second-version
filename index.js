f6092394a2110b2f49bd29db6b7f6456200738b2
Matt0107 <mathieu.durimel@gmail.com>
2023-02-12 23:02:45 +0100

    burger menu created

---
+++ index.js
@@ -0,0 +1,26 @@
+// const burgerMenu = document.querySelector(".burger-menu");
+// const links = document.querySelector(".navbar a");
+
+// burgerMenu.addEventListener("click", () => {
+//   links.classList.toggle("active");
+// });
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerMenuContent = document.querySelector(".burger-menu-content");
+const closeButton = document.querySelector(".close-button");
+
+burgerMenu.addEventListener("click", function () {
+  document.body.classList.toggle("burger-menu-open");
+});
+
+closeButton.addEventListener("click", function () {
+  document.body.classList.remove("burger-menu-open");
+});
+
+document.addEventListener("click", function (event) {
+  if (
+    !event.target.closest(".burger-menu") &&
+    !event.target.closest(".burger-menu-content")
+  ) {
+    document.body.classList.remove("burger-menu-open");
+  }
+});
